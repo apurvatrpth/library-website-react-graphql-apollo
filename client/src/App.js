838fb5650897080e8e1b8d2835af7edb1951eea8
apurvatrpth <at131296@gmail.com>
2020-10-28 05:00:06 +0530

    Add react client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div id='main'>
+      <h1>Library</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
